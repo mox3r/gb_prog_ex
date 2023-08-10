808fd5fd64ca89315a0314d4e519144c4f614940
mox3r <mox3r.box@gmail.com>
2023-08-10 16:23:24 +0300

    bugfix

--- c#_ex/Codewars/Multiple_of_numbers/Program.cs
+++ c#_ex/Codewars/Multiple_of_numbers/Program.cs
@@ -4,12 +4,12 @@ public class Kata
 {
     public static List<int> FindMultiples(int integer, int limit)
     {
-        List<int> list = new ();
-        int j = 0;
+        List<int> list = new List<int>();
+        int j = integer;
         for (int i = 0; j <= limit; i++)
         {
-            list[i] = j;
-            j += integer;
+          list.Add(j);
+          j+= integer;
         }
         return list;
     }
