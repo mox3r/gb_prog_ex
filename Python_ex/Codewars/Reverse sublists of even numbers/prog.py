6e005350f475872826ab7d8a885d89e20d3f9d3a
mox3r <mox3r.box@gmail.com>
2023-08-29 14:37:10 +0300

    reverse even sublist

---
+++ Python_ex/Codewars/Reverse sublists of even numbers/prog.py
@@ -0,0 +1,12 @@
+def rev_sub(arr):
+    res, tmp = [], []
+    for x in arr:
+        if x % 2:
+            res = res+tmp+[x]
+            tmp = []
+        else:
+            tmp = [x]+tmp
+    return res+tmp
+
+
+print(rev_sub([4, 2, 4, 6, 66, 44, 32, 22, 13, 4, 6]))
