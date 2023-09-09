61970a9c9d811fe6fbafb1322c299bf82185d4d2
mox3r <mox3r.box@gmail.com>
2023-09-09 15:29:43 +0300

    HW04

---
+++ Python_ex/GB_seminar_py/Seminar004/32.py
@@ -0,0 +1,23 @@
+# Задача 32: Определить индексы элементов массива (списка), 
+# значения которых принадлежат заданному диапазону 
+# (т.е. не меньше заданного минимума и не больше заданного максимума)
+
+import os
+from random import randint
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+size = int(input('Введите длину массива: '))
+minValue = int(input('Введите минимальное значение диапозона: '))
+maxValue = int(input('Введите максимальное значение диапозона: '))
+
+rand_list = []
+
+for i in range(size):
+    rand_list.append(randint(-1000, 1000))
+    
+print(f'Список случайных значений [-1000; 1000], диапозон поиска [{minValue}; {maxValue}]')
+
+for item in rand_list:
+    if item >= minValue and item <= maxValue:
+        print(f'Значение эл-та {item}, индекс', rand_list.index(item))
