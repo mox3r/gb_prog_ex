9c8b9eadfae0b0f209a829968dcc4d8112c591bc
mox3r <mox3r.box@gmail.com>
2023-09-16 18:59:44 +0300

    pr

--- Python_ex/GB_seminar_py/Seminar006/38.py
+++ Python_ex/GB_seminar_py/Seminar006/38.py
@@ -1,7 +1,9 @@
 import os
 from time import sleep
+from pathlib import Path
 
-f_name = r'data.txt'
+# f_name = Path('GB_seminar_py', 'Seminar006', 'data.txt')
+f_name = 'data.txt'
 
 
 def guide_reader(data_name):
@@ -16,7 +18,8 @@ def guide_write(data_name):
     str_fname = input('Отчество: ')
     str_phone = input('Телефон: ')
 
-    new_guide_str = '\n' + str_surname + '\t' + str_name + '\t' + str_fname + '\t' + '\t' + str_phone
+    new_guide_str = str_surname + ' ' + \
+        str_name + ' ' + str_fname + ' ' + str_phone + '\n'
 
     with open(data_name, 'a', encoding='utf8') as data:
         data.write(new_guide_str)
@@ -30,17 +33,20 @@ def guide_finder(data_name):
                 print(line.strip())
 
 
-def guide_finder_pro(data_name):
-    item_type = int(
-        input('Выберите где искать:\n' + '0. Фамилия\n' + '1. Имя\n' +
-              '2. Отчество\n' + '3. Телефон\n' + 'Выбор: '))
-    item = input('Значение: ')
-
+def sort_by(data_name):
+    list_1 = []
+    item_type = int(input(
+        'Введите тип сортировки\n 0 - фамилия\n 1 - имя\n 2 - отчество\n 3 - телефон\n Выбор: '))
     with open(data_name, 'r', encoding='utf8') as data:
         for line in data:
-            line = line.split()
-            if item.lower() == line[item_type].lower():
-                print(*line)
+            line = line.split(' ')
+            list_1.append(line)
+        list_1.sort(key=lambda person: person[item_type])
+
+    with open(data_name, 'w', encoding='utf8') as data:
+        for item in list_1:
+            item = ' '.join(item)
+            data.write(item.strip() + '\n')
 
 
 while True:
@@ -48,9 +54,9 @@ while True:
 
     print('Телефонный справочник')
     print('1 - Вывести справочник\n' + '2 - Создать новую запись\n' +
-          '3 - Поиск\n' + '4 - Расширенный поиск\n'
+          '3 - Поиск\n' + '4 - Сортировка\n'
           'q - Выйти из программы. ')
-    choise = input('Выберите действие: ')
+    choise = input('Выберите действие: ').lower()
 
     if choise == '1':
         guide_reader(f_name)
@@ -62,9 +68,9 @@ while True:
         guide_finder(f_name)
         input('Нажмите ВВОД чтобы продолжить')
     elif choise == '4':
-        guide_finder_pro(f_name)
+        sort_by(f_name)
         input('Нажмите ВВОД чтобы продолжить')
-    elif choise == 'q' or choise == 'Q':
+    elif choise == 'q':
         break
     else:
         print('Я ваще хз что ты хочешь')
