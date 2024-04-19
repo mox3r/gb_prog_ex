93ad0d3c904a758f15da4790fb356040850a9222
mox3r <mox3r.box@gmail.com>
2024-04-19 12:53:04 +0300

    fixes

--- sql/lesson5.sql
+++ sql/lesson5.sql
@@ -1,46 +1,49 @@
 use lesson_3;
-
-SELECT * FROM staff;
+SELECT *
+FROM staff;
 -- Ранжирование
-SELECT salary, post, concat(first_name, " ", last_name) AS fullname, DENSE_RANK() OVER (
+SELECT salary,
+    post,
+    concat(first_name, " ", last_name) AS fullname,
+    DENSE_RANK() OVER (
         ORDER BY salary DESC
     ) AS 'dense_rank'
 FROM staff;
-
-SELECT salary, post, concat(first_name, " ", last_name) AS fullname, DENSE_RANK() OVER (
-        PARTITION BY
-            post
+SELECT salary,
+    post,
+    concat(first_name, " ", last_name) AS fullname,
+    DENSE_RANK() OVER (
+        PARTITION BY post
         ORDER BY salary DESC
     ) AS 'dense_rank'
 FROM staff;
-
 SELECT *
 FROM (
-        SELECT salary, post, concat(first_name, " ", last_name) AS fullname, DENSE_RANK() OVER (
-                PARTITION BY
-                    post
+        SELECT salary,
+            post,
+            concat(first_name, " ", last_name) AS fullname,
+            DENSE_RANK() OVER (
+                PARTITION BY post
                 ORDER BY salary DESC
             ) AS 'dens_rank'
         FROM staff
     ) rank_staff
-WHERE
-    dens_rank = 1;
-
-SELECT
-    salary,
+WHERE dens_rank = 1;
+SELECT salary,
     post,
-    concat(first_name, " ", last_name) AS fullname,
+    concat(first_name, " ", last_name) AS full_name,
     AVG(salary) OVER w AS avg_salary,
     SUM(salary) OVER w AS sum_salary,
     SUM(salary) OVER w1 AS sum_total_salary,
     ROUND(
-        100 * salary / SUM(salary) OVER w, 2
+        100 * salary / SUM(salary) OVER w,
+        2
     ) AS percent_sum,
     ROUND(
-        100 * salary / SUM(salary) OVER w1, 2
+        100 * salary / SUM(salary) OVER w1,
+        2
     ) AS percent_total_sum
-FROM staff
-WINDOW w AS (
-        PARTITION BY
-            post
-    ), w1 AS ();
+FROM staff WINDOW w AS (PARTITION BY post),
+    w1 AS ();
+
+    
