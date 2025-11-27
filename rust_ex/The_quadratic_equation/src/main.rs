74142051da9ab29cdd888fc5fef967075cecc423
mox3r <mox3r.box@gmail.com>
2025-11-27 15:55:43 +0300

    print fix

--- rust_ex/The_quadratic_equation/src/main.rs
+++ rust_ex/The_quadratic_equation/src/main.rs
@@ -32,19 +32,62 @@ fn main() {
 
         let d: f64 = b * b - 4.0 * a * c;
 
+        fn print_result(a: f64, b: f64, c: f64) {
+            let mut a_sign: String = ' '.to_string();
+            let mut b_sign: String = '+'.to_string();
+            let mut c_sign: String = '+'.to_string();
+
+            let mut a_value: String = a.abs().to_string();
+            let mut b_value: String = b.abs().to_string();
+
+            //a print case
+            if a.abs() == 1.0 {
+                a_value = a_sign
+            }
+            if a < 0.0 {
+                a_sign = '-'.to_string()
+            } else {
+                a_sign = ' '.to_string()
+            };
+
+            //b print case
+            if b == -1.0 {
+                b_value = ' '.to_string();
+                b_sign = '-'.to_string()
+            } else if b == 1.0 {
+                b_value = ' '.to_string()
+            };
+            if b < 0.0 {
+                b_sign = '-'.to_string()
+            };
+
+            //c print case
+            if c < 0.0 {
+                c_sign = '-'.to_string()
+            };
+            println!(
+                "Принимает вид:\n {a_sign}{}x**2 {b_sign} {}x {c_sign} {} = 0",
+                a_value,
+                b_value,
+                c.abs()
+            );
+        }
+
+        print_result(a, b, c);
+
         if d > 0.0 {
             let x1 = (-b + d.sqrt()) / (2.0 * a);
             let x2 = (-b - d.sqrt()) / (2.0 * a);
 
             println!(
-                "Решено, корни уравнения:\nD = {}\nx1 = {}\nx2 = {}",
+                "Решено, корни уравнения:\nD = {}\nx1 = {:.4}\nx2 = {:.4}",
                 d, x1, x2
             );
         }
         if d == 0.0 {
             let x1 = -b / (2.0 * a);
 
-            println!("Решено, корень уравнения:\nD = {}\nx = {}", d, x1);
+            println!("Решено, корень уравнения:\nD = {}\nx = {:.4}", d, x1);
         }
         if d < 0.0 {
             println!("Уравнение не имеет решения, D = {}", d);
